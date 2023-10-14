2d9067f1bc7bd0782663acd53370f88613062bd2
Ollie Treend <ollie@treend.uk>
2023-10-14 16:59:32 +0100

    Add a call to action node

--- src/plugins.js
+++ src/plugins.js
@@ -1,7 +1,25 @@
-import { exampleSetup } from "prosemirror-example-setup"
+import { buildMenuItems, exampleSetup } from "prosemirror-example-setup"
 import { Plugin } from "prosemirror-state"
+import { wrapItem } from "prosemirror-menu"
+
+const buildMenu = (schema) => {
+  const menu = buildMenuItems(schema);
+
+  let node;
+  if (node = schema.nodes.call_to_action) {
+    menu.typeMenu.content.push(
+      wrapItem(node, {
+        title: "Change to call to action",
+        label: "Call to action"
+      })
+    );
+  }
+
+  return menu.fullMenu;
+};
 
 export default (options) => {
+  options.menuContent = buildMenu(options.schema);
   const plugins = exampleSetup(options);
 
   // Remove the "ProseMirror-example-setup-style" class name plugin
--- src/schema.js
+++ src/schema.js
@@ -2,9 +2,20 @@ import { addListNodes } from "prosemirror-schema-list"
 import { Schema } from "prosemirror-model"
 import { schema as basicSchema } from "prosemirror-schema-basic"
 
+let nodes = basicSchema.spec.nodes;
+
+// Call to action
+nodes = nodes.addToEnd('call_to_action', {
+  content: "block+",
+  group: "block",
+  defining: true,
+  parseDOM: [{ tag: "div.call-to-action" }],
+  toDOM() { return ["div", { class: "call-to-action" }, 0] }
+});
+
 // Mix the nodes from prosemirror-schema-list into the basic schema to
 // create a schema with list support.
 export default new Schema({
-  nodes: addListNodes(basicSchema.spec.nodes, "paragraph block*", "block"),
+  nodes: addListNodes(nodes, "paragraph block*", "block"),
   marks: basicSchema.spec.marks,
 });
