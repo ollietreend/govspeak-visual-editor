d7c6459a843b31a98868ea870f665a2af3f43eb1
Ollie Treend <ollie@treend.uk>
2023-10-17 00:30:12 +0100

    Add information callout

---
+++ src/nodes/information_callout.js
@@ -0,0 +1,25 @@
+import { wrapItem } from "prosemirror-menu"
+import { textblockTypeInputRule } from "prosemirror-inputrules"
+
+export const name = "information_callout"
+
+export const schema = {
+  content: "inline*",
+  group: "block",
+  defining: true,
+  parseDOM: [{ tag: `div.application-notice.info-notice[role="note"][aria-label="Information"]` }],
+  toDOM() { return ["div", { class: "application-notice info-notice" }, ["p", 0]] }
+}
+
+export const buildMenu = ({ menu, schema }) => {
+  menu.typeMenu.content.push(
+    wrapItem(schema.nodes[name], {
+      label: "Information callout"
+    })
+  )
+}
+
+export const inputRules = (schema) => ([
+  // ^ Information callout
+  textblockTypeInputRule(/^\^\s$/, schema.nodes[name]),
+])
